ac5f5001ddedc9c553730c197a776cb54dda5781
Harsh Baid <74610712+harshbaid-13@users.noreply.github.com>
2023-04-10 15:53:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello')
+  res.send('Hello Guys! Wassup?')
+})
+app.get('/harsh', function (req, res) {
+  res.send('Hello Guys! Harsh this side!')
 })
 
 app.listen(4000, ()=>{
